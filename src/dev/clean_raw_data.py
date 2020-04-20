7adef2841e44aa62ad41db80c3416d49527f8cfc
OzziAlk <alkhayat@wharton.upenn.edu>
2020-04-20 18:36:30 -0400

    added clean raw data IntegrateData subclass and cleaned head data

--- src/dev/clean_raw_data.py
+++ src/dev/clean_raw_data.py
@@ -1,4 +1,25 @@
-import pandas as pd
+from dev.dev_lib import IntegrateData
 
-data = pd.read_csv("../data/raw_stock_data_HEAD.csv")
-print(data.columns)
+
+class CleanRawData(IntegrateData):
+
+    def __init__(self, in_path, out_path=""):
+        super().__init__(in_path, output_path=out_path)
+
+    def integrate_data(self):
+
+        # adjust for erroneous negative entries into price column
+        self.data['PRC'] = self.data['PRC'].apply(lambda x: abs(x))
+
+        # call to parent method to mark self.data as ready for writing
+        super().integrate_data()
+
+
+def main():
+    path_in = "../data/raw_stock_data_HEAD.csv"
+
+    clean = CleanRawData(path_in)
+    clean.process()
+
+
+main()
